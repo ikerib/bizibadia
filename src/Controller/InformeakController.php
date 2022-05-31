0e13e4c7584dd00319deedcb13333b6f9ab2a07c
ikerib <ikerib@gmail.com>
2022-05-31 13:59:15 +0200

    Informaeak

--- src/Controller/InformeakController.php
+++ src/Controller/InformeakController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Bizikleta;
+use App\Entity\Gunea;
 use App\Entity\Mailegua;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
@@ -16,11 +18,15 @@ class InformeakController extends AbstractController
     {
         $countUsers = $em->getRepository(User::class)->findAll();
         $countRents = $em->getRepository(Mailegua::class)->findAll();
+        $guneak     = $em->getRepository(Gunea::class)->findAll();
+        $bizikletak = $em->getRepository(Bizikleta::class)->findAll();
         $monthly    = $em->getRepository(Mailegua::class)->countByMonth();
 
         return $this->render('informeak/index.html.twig', [
             'users'         => $countUsers,
             'maileguak'     => $countRents,
+            'guneak'        => $guneak,
+            'bizikletak'    => $bizikletak,
             'monthly'       => $monthly
         ]);
     }
