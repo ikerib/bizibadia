55e45e533d9d836408fa2a7f6a12666cf39b4ce7
ikerib <ikerib@gmail.com>
2022-10-04 10:33:51 +0200

    webpack encore recipe updated

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,14 @@ Encore
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
+
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
     })
+
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
-        config.corejs = 3;
+        config.corejs = '3.23';
     })
     .enableSassLoader()
     .autoProvidejQuery()
